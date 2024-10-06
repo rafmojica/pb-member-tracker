82c5e02007ee48fdfdecb817182800ba7b9efd4d
Rafa <rafumojica@gmail.com>
2024-10-05 23:56:32 -0700

    restarted. removed bun (was problematic with tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <nav className="navbar__items">
+      <ul className="navbar__list">
+        <li className="navbar__item">Home</li>
+        <li className="navbar__item">Members</li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default App
